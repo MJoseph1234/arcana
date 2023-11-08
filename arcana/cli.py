fbd493bc65a7d73cc90cccd58a13217a136c58b4
MJoseph1234 <josco.michael@gmail.com>
2023-11-07 21:51:40 -0600

    updating cli command
    runnable from cli

---
+++ arcana/cli.py
@@ -0,0 +1,12 @@
+import argparse
+
+import page_maker
+
+def main():
+	page_maker.build_website()
+
+def add_arguments():
+	pass
+
+if __name__ == '__main__':
+	main
--- arcana/page_maker.py
+++ arcana/page_maker.py
@@ -1,68 +1,10 @@
 import markdown
 import os
+import shutil
 
-from . import settings
+import settings
 
 from templater import fill_template
-
-# def fill_template(new_page, template_page, **kwargs):
-# 	with open(template_page, 'r') as template, open(new_page, 'w') as new:
-# 		for count, text in enumerate(template):
-# 			if "{{" in text:
-# 				handle_statement(text, new, **kwargs)
-# 				continue
-# 			if "{%" in text:
-# 				handle_block(text, new, **kwargs)
-# 				continue
-# 			new.write(text)
-
-# def handle_statement(text, new, **kwargs):
-# 	pre, temp = text.split("{{")
-# 	variable, post = temp.split("}}")
-# 	variable = variable.strip()
-
-# 	indent = pre.count('\t') * '\t'
-# 	if kwargs.get('indent'):
-# 		indent += kwargs['indent']
-
-# 	var = None
-# 	if kwargs.get(variable) is not None:
-# 		if isinstance(kwargs[variable], str):
-# 			var = kwargs[variable]
-# 		elif isinstance(kwargs[variable], list):
-# 			var = f'{indent}'.join(kwargs[variable])
-	
-# 	if not new:
-# 		if var is not None:
-# 			return(pre + var + post)
-# 		else:
-# 			return(pre + post)
-# 	else:
-# 		if var is not None:
-# 			new.write(pre + var + post)
-# 		else:
-# 			new.write(pre + post)
-
-# def handle_block(text, new, **kwargs):
-# 	"""Right now, blocks won't be able to have their own nested block
-# 	"""
-# 	pre, temp = text.split("{%")
-# 	variable, post = temp.split("%}")
-# 	variable = variable.strip()
-
-# 	indent = pre.count('\t') * '\t'
-# 	if kwargs.get('indent'):
-# 		indent += kwargs['indent']
-
-# 	with open('templates/' + variable + '.html', 'r') as block:
-# 		for count, text in enumerate(block):
-
-# 			if "{{" in text:
-# 				handle_statement(text, new, indent = indent, **kwargs)
-# 				continue
-			
-# 			new.write(f'{indent}{text}')
-# 		new.write(post)
 
 def navbar_needs_update():
 	""" Check if the navbar should be re-built by 
@@ -71,18 +13,15 @@ def navbar_needs_update():
 
 	If the list of md files matches the list of html files, no update needed
 	"""
-	markdown_folder = 'markdown/'
-	generated_html_folder = 'generated_html/'
-
 	md_files = set()
-	for file in os.listdir(markdown_folder):
+	for file in os.listdir(settings.MARKDOWN):
 		file_name, ext = os.path.splitext(file)
 		if ext not in {'.txt', '.md'}:
 			continue
 		md_files.add(file_name)
 
 	html_files = set()
-	for file in os.listdir(generated_html_folder):
+	for file in os.listdir(settings.OUTPUT):
 		file_name, ext = os.path.splitext(file)
 		if ext != '.html':
 			continue
@@ -91,21 +30,18 @@ def navbar_needs_update():
 	return(html_files != md_files)
 
 def get_pages_to_update():
-	markdown_folder = 'markdown/'
-	generated_html_folder = 'generated_html/'
-	
 	needs_update = []
-	for md_file in os.listdir(markdown_folder):
+	for md_file in os.listdir(settings.MARKDOWN):
 		file_name, ext = os.path.splitext(md_file)
 		if ext not in {'.txt', '.md'}:
 			continue
 
-		html_file = os.path.join(generated_html_folder, file_name + '.html')
+		html_file = os.path.join(settings.OUTPUT, file_name + '.html')
 		if not os.path.isfile(html_file):
 			needs_update.append(md_file)
 			continue
 
-		if os.path.getmtime(os.path.join(markdown_folder, md_file)) > os.path.getmtime(html_file):
+		if os.path.getmtime(os.path.join(settings.MARKDOWN, md_file)) > os.path.getmtime(html_file):
 			needs_update.append(md_file)
 
 	print(f'needs update: {needs_update}')
@@ -114,17 +50,18 @@ def get_pages_to_update():
 def capitalize_words(string):
 	return(' '.join(s.capitalize() for s in string.split()))
 
+def add_static_files_to_output():
+	for file in os.listdir(settings.STATIC):
+		shutil.copy(os.path.join(settings.STATIC, file), settings.OUTPUT)
+
 def build_website():
 	"""Build the entire website from all the files in
 	the markdown folder"""
-	markdown_folder = 'markdown/'
-	generated_html_folder = 'generated_html/'
-	templates = 'templates/'
 
 	page_links = {}
 
 	# Build the list of page links for the top navbar
-	for file in os.listdir(markdown_folder):
+	for file in os.listdir(settings.MARKDOWN):
 		file_name, ext = os.path.splitext(file)
 		if ext not in {'.txt', '.md'}:
 			continue
@@ -133,7 +70,7 @@ def build_website():
 			page_links[file_name] = f'<a class="split" href="{file_name}.html">{capitalize_words(file_name)}</a>\n'
 
 	# Build html pages based on the files in the Markdown folder
-	for file in os.listdir(markdown_folder):
+	for file in os.listdir(settings.MARKDOWN):
 		file_name, ext = os.path.splitext(file)
 		if ext not in {'.txt', '.md'}:
 			continue
@@ -155,15 +92,20 @@ def build_website():
 			else:
 				p.append(page_links[line])
 
-
-		with open(f'markdown/{file}') as input_file:
+		markdown_file = os.path.join(settings.MARKDOWN, file)
+		with open(markdown_file) as input_file:
 			text = input_file.read()
 		content = markdown.markdown(text, extensions=['tables', 'nl2br']).splitlines(True)
 
-		fill_template(f'generated_html/{file_name}.html', 'templates/base.html', 
+		output_file = os.path.join(settings.OUTPUT, file_name + '.html')
+		base_template = os.path.join(settings.TEMPLATES, 'base.html')
+
+		fill_template(output_file, base_template, 
 					page_title = page_title,
 					content = content,
 					page_links = p)
+
+	add_static_files_to_output()
 
 if __name__ == '__main__':
 	build_website()
